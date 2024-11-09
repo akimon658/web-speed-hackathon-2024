671e9d151326d5ae6d4491c60717a150d98d1e3c
Takumi Akimoto <81888693+akimon658@users.noreply.github.com>
2024-11-09 22:52:31 +0900

    カバー画像の前処理

--- workspaces/app/src/pages/TopPage/internal/CoverSection.tsx
+++ workspaces/app/src/pages/TopPage/internal/CoverSection.tsx
@@ -5,8 +5,6 @@ import { Link } from '../../../foundation/components/Link';
 import { Text } from '../../../foundation/components/Text';
 import { Color, Radius, Space, Typography } from '../../../foundation/styles/variables';
 
-import { HeroImage } from './HeroImage';
-
 const _Wrapper = styled.div`
   width: calc(100% + ${Space * 4}px);
   margin-left: -${Space * 2}px;
@@ -29,10 +27,21 @@ const _SearchLink = styled(Link)`
   transform: translateY(50%);
 `;
 
+const _ImgWrapper = styled.div`
+  width: 100%;
+`;
+
+const _Image = styled.img`
+  display: inline-block;
+  width: 100%;
+`;
+
 export const CoverSection: React.FC = () => {
   return (
     <_Wrapper>
-      <HeroImage />
+      <_ImgWrapper>
+        <_Image alt='Cyber TOON' src='/assets/hero.avif' />
+      </_ImgWrapper>
       <_SearchLink href="/search">
         <SvgIcon color={Color.MONO_A} height={24} type="Search" width={24} />
         <Text color={Color.MONO_A} typography={Typography.NORMAL16}>
