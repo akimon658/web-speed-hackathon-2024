a44bf265c549adae2a8f9e7d935e3eb8d1636288
Takumi Akimoto <81888693+akimon658@users.noreply.github.com>
2024-11-10 16:37:03 +0900

    全キャッシュ

--- workspaces/server/src/middlewares/cacheControlMiddleware.ts
+++ workspaces/server/src/middlewares/cacheControlMiddleware.ts
@@ -2,11 +2,6 @@ import { createMiddleware } from 'hono/factory';
 
 export const cacheControlMiddleware = createMiddleware(async (c, next) => {
   await next();
-  const url = new URL(c.req.url)
-  const path = url.pathname
-  if (path.startsWith('/image')) {
-    c.res.headers.append('Cache-Control', 'public');
-    c.res.headers.append('Cache-Control', 'max-age=31536000');
-    return
-  }
+  c.res.headers.append('Cache-Control', 'public');
+  c.res.headers.append('Cache-Control', 'max-age=31536000');
 });
