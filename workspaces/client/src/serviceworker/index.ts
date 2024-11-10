814a29c2de0fe5f7159dde994f87a288da15d4a0
Takumi Akimoto <81888693+akimon658@users.noreply.github.com>
2024-11-10 09:53:59 +0900

    jitter、queueを消す

--- workspaces/client/src/serviceworker/index.ts
+++ workspaces/client/src/serviceworker/index.ts
@@ -1,12 +1,4 @@
 /// <reference types="@types/serviceworker" />
-import PQueue from 'p-queue';
-
-import { jitter } from './jitter';
-
-// ServiceWorker が負荷で落ちないように並列リクエスト数を制限する
-const queue = new PQueue({
-  concurrency: 5,
-});
 
 self.addEventListener('install', (ev: ExtendableEvent) => {
   ev.waitUntil(self.skipWaiting());
@@ -15,18 +7,3 @@ self.addEventListener('install', (ev: ExtendableEvent) => {
 self.addEventListener('activate', (ev: ExtendableEvent) => {
   ev.waitUntil(self.clients.claim());
 });
-
-self.addEventListener('fetch', (ev: FetchEvent) => {
-  ev.respondWith(
-    queue.add(() => onFetch(ev.request), {
-      throwOnTimeout: true,
-    }),
-  );
-});
-
-async function onFetch(request: Request): Promise<Response> {
-  // サーバーの負荷を分散するために Jitter 処理をいれる
-  await jitter();
-
-  return await fetch(request);
-}
